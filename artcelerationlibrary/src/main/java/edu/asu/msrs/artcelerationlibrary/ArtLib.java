48763019f3583c485db458039a9343fd820c1145
canhunan1 <jianan205@gmail.com>
2016-11-04 18:42:34 -0700

    Use bitmap in the memory file

--- artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/ArtLib.java
+++ artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/ArtLib.java
@@ -14,8 +14,8 @@ import android.os.Messenger;
 import android.os.ParcelFileDescriptor;
 import android.os.RemoteException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,10 +30,12 @@ public class ArtLib {
     private boolean mBound;
     static final int MSG_HELLO = 0;
     static final int MSG_MULTI = 1;
-    public ArtLib(Activity activity){
+
+    public ArtLib(Activity activity) {
         mActivity = activity;
         init();
     }
+
     ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -47,42 +49,44 @@ public class ArtLib {
             mBound = false;
         }
     };
-    public void init(){
-        mActivity.bindService(new Intent(mActivity, TransformService.class),mServiceConnection, Context.BIND_AUTO_CREATE);
+
+    public void init() {
+        mActivity.bindService(new Intent(mActivity, TransformService.class), mServiceConnection, Context.BIND_AUTO_CREATE);
     }
 
-    public String[] getTransformsArray(){
+    public String[] getTransformsArray() {
         String[] transforms = {"Gaussian Blur", "Neon edges", "Color Filter"};
         return transforms;
     }
 
-    public TransformTest[] getTestsArray(){
+    public TransformTest[] getTestsArray() {
         TransformTest[] transforms = new TransformTest[3];
-        transforms[0]=new TransformTest(0, new int[]{1,2,3}, new float[]{0.1f, 0.2f, 0.3f});
-        transforms[1]=new TransformTest(1, new int[]{11,22,33}, new float[]{0.3f, 0.2f, 0.3f});
-        transforms[2]=new TransformTest(2, new int[]{51,42,33}, new float[]{0.5f, 0.6f, 0.3f});
+        transforms[0] = new TransformTest(0, new int[]{1, 2, 3}, new float[]{0.1f, 0.2f, 0.3f});
+        transforms[1] = new TransformTest(1, new int[]{11, 22, 33}, new float[]{0.3f, 0.2f, 0.3f});
+        transforms[2] = new TransformTest(2, new int[]{51, 42, 33}, new float[]{0.5f, 0.6f, 0.3f});
 
         return transforms;
     }
 
-    public void registerHandler(TransformHandler artlistener){
-        this.artlistener=artlistener;
+    public void registerHandler(TransformHandler artlistener) {
+        this.artlistener = artlistener;
     }
 
-    public boolean requestTransform(Bitmap img, int index, int[] intArgs, float[] floatArgs){
+    public boolean requestTransform(Bitmap img, int index, int[] intArgs, float[] floatArgs) {
         try {
-            MemoryFile memoryFile = new MemoryFile("someone",10);
-            String source = "testSt";
-            byte[] byteArray = source.getBytes();
-            memoryFile.writeBytes(byteArray, 0, 0, source.length());
-
-            InputStream is = memoryFile.getInputStream();
-            System.out.println("is is"+is.read());
-            ParcelFileDescriptor pfd =  MemoryFileUtil.getParcelFileDescriptor(memoryFile);
+            //Write the image to the memory file
+            //Firstly,convert bitmap to byte array
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            img.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            //Secondly, put the stream into the memory file.
+            MemoryFile memoryFile = new MemoryFile("someone", byteArray.length);
+            memoryFile.writeBytes(byteArray, 0, 0, byteArray.length);
+            ParcelFileDescriptor pfd = MemoryFileUtil.getParcelFileDescriptor(memoryFile);
             int what = MSG_MULTI;
             Bundle dataBundle = new Bundle();
-            dataBundle.putParcelable("pfd",pfd);
-            Message msg = Message.obtain(null,what,2,3);
+            dataBundle.putParcelable("pfd", pfd);
+            Message msg = Message.obtain(null, what, 2, 3);
             msg.setData(dataBundle);
             try {
                 mMessenger.send(msg);
@@ -94,7 +98,6 @@ public class ArtLib {
         }
         return true;
     }
-
 
 
     public void testPurge() throws Exception {
--- artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/TransformService.java
+++ artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/TransformService.java
@@ -2,6 +2,8 @@ package edu.asu.msrs.artcelerationlibrary;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -10,13 +12,13 @@ import android.os.Messenger;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 
 public class TransformService extends Service {
 
-    public void onCreate(){
-        Log.v(TAG,"test");
+    public void onCreate() {
+        Log.v(TAG, "test");
     }
 
     public TransformService() {
@@ -25,32 +27,27 @@ public class TransformService extends Service {
     static String TAG = "ArtTransformService";
     static final int MSG_HELLO = 0;
     static final int MSG_MULTI = 1;
+
     static class ArtTransformHandler extends Handler {
         @Override
         public void handleMessage(Message msg) {
             Log.d(TAG, "handleMessage(msg)");
-            switch(msg.what){
+            switch (msg.what) {
                 case MSG_HELLO:
                     Log.d(TAG, "Hello!");
-                        break;
+                    break;
                 case MSG_MULTI:
                     Bundle dataBundle = msg.getData();
                     ParcelFileDescriptor pfd = (ParcelFileDescriptor) dataBundle.get("pfd");
-                    //FileInputStream fis = new FileInputStream(pfd);
-                    FileInputStream fis = new ParcelFileDescriptor.AutoCloseInputStream(pfd);
-                    StringBuilder builder = new StringBuilder();
-                    //String sm = new String(fis);
-                    int ch;
-                    try {
-                        while((ch = fis.read()) != -1){
-                            builder.append((char)ch);
-                            System.out.println(ch);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    int result = msg.arg1+msg.arg2;
-                    Log.d(TAG, builder.toString());
+                    InputStream istream = new ParcelFileDescriptor.AutoCloseInputStream(pfd);
+                    //convertInputStreamToBitmap
+                    Bitmap img = BitmapFactory.decodeStream(istream);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                    img.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    byte[] byteArray = stream.toByteArray();
+                    //Log.v(TAG, byteArray.toString());
+                    int result = msg.arg1 + msg.arg2;
                     Log.d(TAG, "Multi" + result);
                     break;
                 default:
