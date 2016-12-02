ad61e8fe7d8a44295889fc3e79e0b68abd34c435
canhunan1 <jianan205@gmail.com>
2016-12-01 23:49:39 -0700

    Added comment in the NeonEdge.java

--- artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/NeonEdge.java
+++ artcelerationlibrary/src/main/java/edu/asu/msrs/artcelerationlibrary/NeonEdge.java
@@ -1,17 +1,22 @@
+/**
+ * Created by Jianan on 11/30/2016.
+ * This file is created to do neon edges transform.
+ * Firstly do the sobel edge transform, secondly do the gaussian transform
+ * Thirdly do the linear sum.
+ */
+
 package edu.asu.msrs.artcelerationlibrary;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
 
-/**
- * Created by Jianan on 11/30/2016.
- */
-
 public class NeonEdge {
-    NeonEdge(){
-
-    }
-
+    NeonEdge(){}
+    /*
+    * This function is used to do NeonEdgeTransform
+    * @param img    img is a bitmap to be transformed
+    * @param argsF   argsF is float array with input value for NeonEdgeTransform
+    * */
     public static Bitmap NeonEdgeTransForm(Bitmap img, float argsF[]){
 
         Bitmap original = Bitmap.createBitmap(img);
@@ -21,12 +26,15 @@ public class NeonEdge {
         float[] f = {argsF[0]};
         GaussianBlur gaussianBlur=new GaussianBlur(img, r,f);
         img = gaussianBlur.startTransform();
-        //NativeTransform.NeonEdgeTransform(original, img, argsF[1], argsF[2]);
-
         neonEdgeLinearSum(img, argsF, original);
         return img;
     }
-
+    /*
+    * This function is used to do LinearSum which is the third step of the transform
+    * @param img        img is a bitmap after processed the first and second step
+    * @param argsF      argsF is float array with input value for NeonEdgeTransform
+    * @param original   original is a bitmap to be transformed
+    * */
     private static void neonEdgeLinearSum(Bitmap img, float[] argsF, Bitmap original) {
         for(int i = 0;i < img.getWidth();i++){
             for(int j = 0; j < img.getHeight();j++){
@@ -43,7 +51,6 @@ public class NeonEdge {
                 int PBlue = (int)(blue*argsF[1] + blueOri*argsF[2]);
                 int color = Color.argb(255, Pred, PGreen,PBlue);
                 img.setPixel(i,j,color);
-
             }
         }
     }
